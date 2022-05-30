5284ba83679352ce19483fd2e7309ba6e772bd85
ArsalanAhmed92 <103914524+ArsalanAhmed92@users.noreply.github.com>
2022-05-30 11:10:59 +0500

    first comment

---
+++ app.js
@@ -0,0 +1,15 @@
+// var a = +prompt("enter index no. of food items");
+
+// var food = ["biryani","qourma","karhai","kattakat","tikka","shami","zinger","coldrink"];
+// food.splice(a,1);
+// console.log(food);
+
+
+
+
+
+ var a = +prompt("enter index no. of food items");
+
+ var food = ["biryani","qourma","karhai","kattakat","tikka","shami","zinger","coldrink"];
+ food.splice(a,1);
+ console.log(food);
